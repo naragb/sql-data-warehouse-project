67c6a30813ec0699ded7ae4bfeeb627a90a705bc
Nara <168982560+naragb@users.noreply.github.com>
2025-12-24 22:54:26 -0500

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -41,6 +41,61 @@ SELECT
 FROM silver.crm_cust_info
 WHERE cst_key != TRIM(cst_key);
 
+-- Data Standartization & consistency
+SELECT DISTINCT cst_marital_status
+FROM silver.crm_cust_info
+
+-- ========================================================================
+-- Checking 'silver.crm_prd_info'
+-- ========================================================================
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Results
+SELECT
+   prd_id, 
+   COUNT(*)
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+-- Check for Unwanted Spaces
+-- Expectation: No Result
+SELECT prd_nm
+FROM silver.crm.prd.info
+WHERE prd_nm != TRIM(prd_nm);
+
+-- Check for NULLs or Negative Values in Cost
+-- Expectation: No Result
+SELECT prd_cost
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL;
+
+-- Data Standartization & Consistency
+SELECT Distinct prd_line
+FROM silever.crm_prd_info;
+
+-- Check for Invalid Date Orders(Start Date > End Date)
+-- Expectation: No Results
+SELECT *
+FROM silver.crm_prd_info;
+
+-- ==========================================================================
+-- Checking 'silver.crm_sales_details'
+-- ==========================================================================
+-- Check for Invalid Dates
+-- Expectation: No Invalid Dates
+SELECT 
+   NULLIF(sls_due_dt, 0) AS sls_due_dt
+FROM bronze.crm_sales_details
+WHERE
+    sls_due_dt <= 0
+ OR LEN(sls_due_dt) != 8
+ OR sls_due_dt > 20500101
+ OR sls_due_dt < 19000101;
+
+-- Check for Invalid Date Orders(Order Date > Shipping/Due Dates) 
+
+
+
 
 
 
